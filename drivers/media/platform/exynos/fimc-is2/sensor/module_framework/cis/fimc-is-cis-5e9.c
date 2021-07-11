LineageOS/android_kernel_samsung_gta4xl

drivers: media: platform: exynos: fimc-is2: fix section mismatch warnings
Change-Id: I4428ec9694aedcad2b05382cd392abe3394fa5b0

drivers/media/platform/exynos/fimc-is2/sensor/module_framework/cis/fimc-is-cis-5e9.c
@@ -1977,7 +1977,7 @@ static const struct i2c_device_id sensor_cis_5e9_idt[] = {
 	{},
 };
 
-static struct i2c_driver sensor_cis_5e9_driver = {
+static struct i2c_driver __init sensor_cis_5e9_driver = {
 	.probe	= cis_5e9_probe,
 	.driver = {
 		.name	= SENSOR_NAME,

aleerazagujjar/Destrictize_9611-

drivers/media/platform/exynos/fimc-is2/sensor/module_framework/cis/fimc-is-cis-5e9.c
@@ -1977,7 +1977,7 @@ static const struct i2c_device_id sensor_cis_5e9_idt[] = {
 	{},
 };
 
-static struct i2c_driver sensor_cis_5e9_driver = {
+static struct i2c_driver __refdata sensor_cis_5e9_driver = {
 	.probe	= cis_5e9_probe,
 	.driver = {
 		.name	= SENSOR_NAME,